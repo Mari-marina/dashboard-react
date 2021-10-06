fde00c2d50f5287d46538448d249b59b41c7991d
Mari-marina <shunkina9292@mail.ru>
2021-10-06 23:34:23 +0300

    btn

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import style from "./Navigation.module.css";
-import item1 from "./../../assets/images/item1.svg";
+// import item1 from "./../../assets/images/item1.svg";
 import item2 from "./../../assets/images/item2.svg";
 import item3 from "./../../assets/images/item3.svg";
 import item4 from "./../../assets/images/item4.svg";
@@ -12,7 +12,14 @@ function Navigation(props) {
       <ul className={style.list}>
         <li className={style.item}>
           <button className={style.btn}>
-            <img className={style.navImg} src={item1} alt="" />
+            <svg
+              className={style.navImg}
+              width="24"
+              height="23"
+              viewBox="0 0 24 23"
+            >
+              <path d="M24 12.0534L12 0L0 12.0534H2.31282V23H10.7891V15.7023H13.2109V23H21.6872V12.0534H24ZM8.36731 13.2697V20.5674H4.73461V10.7277L12 3.42993L19.2654 10.7277V20.5674H15.6327V13.2697H13.2109H10.7891H8.36731Z" />
+            </svg>
             <p className={style.navText}>Home</p>
           </button>
         </li>
